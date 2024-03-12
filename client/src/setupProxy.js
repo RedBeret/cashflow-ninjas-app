8492f7de6a2b902f32fee6dba27b14cf3ea2de4c
Steven Espinoza <espisteven5@gmail.com>
2024-03-11 20:48:55 -0700

    updated python server added authentication, seed file, models, app.py, support guide, ai chat messages, middleware js to run two servers and dbs

---
+++ client/src/setupProxy.js
@@ -0,0 +1,27 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    // Proxy endpoints to the Ruby on Rails server on port 3000
+    app.use(
+        "/api/rails/*",
+        createProxyMiddleware({
+            target: "http://localhost:3000",
+            changeOrigin: true,
+            pathRewrite: {
+                "^/api/rails": "/api",
+            },
+        })
+    );
+
+    // Proxy endpoints to the Flask server on port 5555
+    app.use(
+        "/api/flask/*",
+        createProxyMiddleware({
+            target: "http://localhost:5555",
+            changeOrigin: true,
+            pathRewrite: {
+                "^/api/flask": "/api",
+            },
+        })
+    );
+};
